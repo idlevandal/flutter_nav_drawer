b1dfd0915fc844901ea709e21925746a0559a644
David Allen <davetvmusic@hotmail.com>
2021-01-16 17:46:54 +1000

    added some rounded icon buttons. Added navigator pop to settings page.

--- lib/settings.dart
+++ lib/settings.dart
@@ -11,7 +11,38 @@ class Settings extends StatelessWidget {
       drawer: MainDrawer(),
       body: Container(
         child: Center(
-          child: Text('Settings', style: TextStyle(fontSize: 26.0)),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text('Settings', style: TextStyle(fontSize: 26.0)),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                    shape: CircleBorder(),
+                    primary: Colors.blue
+                ),
+                child: Container(
+                  width: 50,
+                  height: 50,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(shape: BoxShape.circle),
+                  child: Icon(Icons.arrow_back_ios_sharp, size: 22.0,)
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                },
+              ),
+              SizedBox(height: 14.0),
+              MaterialButton(
+                color: Colors.blue,
+                shape: CircleBorder(),
+                onPressed: () {},
+                child: Padding(
+                  padding: const EdgeInsets.all(14),
+                  child: Icon(Icons.arrow_back_ios_sharp, size: 22.0, color: Colors.white,)
+                ),
+              )
+            ],
+          ),
         ),
       ),
     );
